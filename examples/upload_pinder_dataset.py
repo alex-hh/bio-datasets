e219b09ef1c1484e81474e63cb4209d9a2ff99ab
alex-hh <alex.hhooker@gmail.com>
2024-10-20 21:48:55 +0100

    complete pinder dataset util

--- examples/upload_pinder_dataset.py
+++ examples/upload_pinder_dataset.py
@@ -3,29 +3,44 @@
 What we really want to upload for each protein is a single ProteinComplex feature,
 and two sets of Protein features for each ligand and each receptor
 (representing the unbound and - where available - predicted unbound states).
-The chains should all be pre-aligned to either the native complex or the predicted unbound entry using
-the pinder utils so that they are all the same size, with missing coordinates masked out.
+The chains should all be pre-aligned to the native complex.
 
-If we choose to pre-align to the native complex, we need to also return full sequences
-and mappings to native complex from full sequences.
-
-To store the structures - we could even drop the missing coordinates and rey on the numbering
+To store the structures - we could even drop the missing coordinates and rely on the numbering
 being the same.
 
-Not sure if we also want to superimpose? Might be a bit dangerous to superimpose
-the unbound complex jointly onto the bound complex: this leaks information.
-
 We should also upload a raw version where we don't do any alignment or masking.
+
+TODO: handle 'canonical' vs non-canonical apo conformation:
+https://github.com/pinder-org/pinder/blob/9c70a92119b844d0d20e35f483b4f1f26b2899c4/src/pinder-core/pinder/core/index/system.py#L89
+
+Q: what is difference between create_complex and create_masked_bound_unbound_complexes?
+
+N.B. can use PinderSystem.download_entry to download files for single system.
+
+Oligomeric state of the protein complex (homodimer, heterodimer, oligomer or higher-order complexes)
+Structure determination method (X-Ray, CryoEM, NMR)
+Resolution
+Interfacial gaps, defined as structurally-unresolved segments on PPI interfaces
+Number of distinct atom types. Many earlier Cryo-EM structures contain only a few atom-types such as only Cα or backbone atoms
+Whether the interface is likely to be a physiological or crystal contact, annotated using Prodigy
+Structural elongation, defined as the maximum variance of coordinates projected onto the largest principal component. This allows detection of long end-to-end stacked complexes, likely to be repetitive with small interfaces
+Planarity, defined as deviation of interfacial Cα atoms from the fitted plane. This interface characteristic quantifies interfacial shape complementarity. Transient complexes have smaller and more planar interfaces than permanent and structural scaffold complexes
+Number of components, defined as the number of connected components of a 10Å Cα radius graph. This allows detection of structurally discontinuous domains
+Intermolecular contacts (labeled as polar or apolar)
 """
 import argparse
-from typing import List
+import pathlib
+from typing import List, Optional
 
 import biotite.sequence.align as align
 import numpy as np
 from biotite import structure as bs
-from datasets import Dataset, Features, Value
+from biotite.structure.residues import get_residue_starts
+from datasets import Array1D, Dataset, Features, Value
 from pinder.core import PinderSystem, get_index, get_metadata
+from pinder.core.index.utils import IndexEntry
 from pinder.core.loader.structure import Structure
+from pinder.core.structure import surgery
 from pinder.core.structure.atoms import (
     _align_and_map_sequences,
     _get_seq_aligned_structures,
@@ -154,9 +169,21 @@ def get_subject_positions_in_ref_masks(
 
 
 class PinderDataset:
-    def __init__(self, index, metadata):
+    def __init__(
+        self,
+        index,
+        metadata,
+        download: bool = False,
+        dataset_path: Optional[str] = None,
+        remove_differing_atoms: bool = True,
+    ):
         self.index = index
         self.metadata = metadata
+        self.download = download
+        self.dataset_path = (
+            pathlib.Path(dataset_path) if dataset_path is not None else None
+        )
+        self.remove_differing_atoms = remove_differing_atoms
 
     def get_aligned_structures(
         self,
@@ -165,7 +192,6 @@ class PinderDataset:
         mode: str = "ref",  # "ref" or "intersection"
     ):
         """TODO: test that we get same result as applying get_seq_aligned_structure
-
 
         Source: https://github.com/pinder-org/pinder/blob/8ad1ead7a174736635c13fa7266d9ca54cf9f44e/src/pinder-core/pinder/core/loader/structure.py#L146
         """
@@ -195,20 +221,7 @@ class PinderDataset:
 
         elif mode == "intersection":
             # handles masking and renumbering
-            ref_mask, target_mask = _get_seq_aligned_structures(ref_at, target_at)
-            # handle differing atoms later to be consistent across all cases
-            # equivalent to align_common_sequence with remove_differing_atoms True and remove_differing_annotations True
-            # ref_at_mod = ref_at.copy()
-            # target_at_mod = target_at.copy()
-            # # remove differing atoms:
-            # ref_target_mask = bs.filter_intersection(ref_at_mod, target_at_mod)
-            # target_ref_mask = bs.filter_intersection(target_at_mod, ref_at_mod)
-            # # remove differing annotations:
-            # ref_at_mod, target_at_mod = bs.surgery.fix_annotation_mismatch(
-            #     ref_at_mod, target_at_mod, ["element", "ins_code", "b_factor"]
-            # )
-            # ref_at = ref_at_mod[ref_target_mask].copy()
-            # target_at = target_at_mod[target_ref_mask].copy()
+            ref_at, target_at = _get_seq_aligned_structures(ref_at, target_at)
 
         else:
             raise ValueError(f"Invalid mode: {mode}")
@@ -228,11 +241,42 @@ class PinderDataset:
             atom_array=target_at,
             pdb_engine=target_struct.pdb_engine,
         )
+
         return ref_struct, target_struct
 
-    def make_structures(
-        self, system: PinderSystem, remove_differing_atoms: bool = True
-    ):
+    def _make_atoms_consistent(self, ref_struct, *structs):
+        # An atom is defined as existent in the second array, if there is an
+        # atom in the second array that has the same annotation values in all
+        # categories that exists in both arrays.
+
+        # So pinder handles this by first modifying annotations to match,
+        # then applying the atom mask, then optionally preserving the modified annotations.
+
+        # It's clear that we don't need to preserve matched b_factors, for example.
+        # although this does require care when applying filter_intersection downstream.
+
+        # Even if atom counts are identical, annotation categories must be the same
+        # First modify annotation arrays to use struc.filter_intersection,
+        # then filter original structure with annotations to match res_id, res_name, atom_name
+        # of intersecting structure
+        ref_at_mod = ref_struct.atom_array.copy()
+        target_at_mods = []
+        ref_target_mask = np.ones(len(ref_at_mod), dtype=bool)
+        for struct in structs:
+            target_at_mod = struct.atom_array.copy()
+            ref_target_mask &= bs.filter_intersection(ref_at_mod, target_at_mod)
+            target_at_mods.append(target_at_mod)
+            # just get rid of any annotations that don't match
+            ref_at_mod, target_at_mod = surgery.fix_annotation_mismatch(
+                ref_at_mod, target_at_mod, ["element", "ins_code", "b_factor"]
+            )
+
+        ref_struct.atom_array = ref_struct.atom_array[ref_target_mask]
+        for target_at_mod, struct in zip(target_at_mods, structs):
+            target_ref_mask = bs.filter_intersection(target_at_mod, ref_at_mod)
+            struct.atom_array = struct.atom_array[target_ref_mask].copy()
+
+    def make_structures(self, system: PinderSystem):
         """We have to choose which reference to align to: choices are complex, apo (unbound) or predicted (unbound).
 
         Bound makes most sense I think.
@@ -240,7 +284,7 @@ class PinderDataset:
         has_apo = system.entry.apo_R and system.entry.apo_L
         has_pred = system.entry.predicted_R and system.entry.predicted_L
 
-        # apo_complex = system.create_apo_complex() - his superimposes structures, which gives info away about interaction
+        # apo_complex = system.create_apo_complex() - this superimposes structures, which gives info away about interaction
         # https://github.com/pinder-org/pinder/blob/8ad1ead7a174736635c13fa7266d9ca54cf9f44e/examples/pinder-system.ipynb
         if has_apo:
             apo_R, apo_L = system.apo_receptor, system.apo_ligand
@@ -275,15 +319,72 @@ class PinderDataset:
                 mode="ref",
             )
 
+        if self.remove_differing_atoms:
+            self._make_atoms_consistent(system.native_R, system.apo_R, system.pred_R)
+            self._make_atoms_consistent(system.native_L, system.apo_L, system.pred_L)
+
         native = system.native_R + system.native_L
         # TODO: add uniprot seq and mapping to native
+        assert len(system.holo_receptor.atom_array) == len(system.native_R.atom_array)
+        assert len(system.holo_ligand.atom_array) == len(system.native_L.atom_array)
         return {
             "complex": native,
             "apo_receptor": apo_R,
             "apo_ligand": apo_L,
             "pred_receptor": pred_R,
             "pred_ligand": pred_L,
+            "holo_receptor": system.holo_receptor,
+            "holo_ligand": system.holo_ligand,
         }
+
+    def __getitem__(self, idx):
+        row = self.index.iloc[idx]
+        metadata = self.metadata[self.metadata["id"] == row["id"]].iloc[0]
+        # n.b. PinderSystem will automatically download if entry can't be found locally
+        # TODO: if necessary, renumber reference ids to always be contiguous (before alignment)
+        system = PinderSystem(
+            entry=IndexEntry(**row.to_dict()), dataset_path=self.dataset_path
+        )
+        if system.entry.predicted_R:
+            uniprot_seq_R = system.pred_receptor.sequence
+        else:
+            uniprot_seq_R = None
+        if system.entry.predicted_L:
+            uniprot_seq_L = system.pred_ligand.sequence
+        else:
+            uniprot_seq_L = None
+        structures = self.make_structures(system)
+        receptor_res_starts = get_residue_starts(system.holo_receptor.atom_array)
+        ligand_res_starts = get_residue_starts(system.holo_ligand.atom_array)
+        structures["receptor_uniprot_resids"] = [
+            structures.pop("holo_receptor").resolved_pdb2uniprot[res_id]
+            for res_id in system.holo_receptor.atom_array[receptor_res_starts].res_id
+        ]
+        structures["ligand_uniprot_resids"] = [
+            structures.pop("holo_ligand").resolved_pdb2uniprot[res_id]
+            for res_id in system.holo_ligand.atom_array[ligand_res_starts].res_id
+        ]
+        structures["receptor_uniprot_accession"] = system.entry.uniprot_R
+        structures["ligand_uniprot_accession"] = system.entry.uniprot_L
+        # TODO: get metadata
+        structures["receptor_uniprot_seq"] = uniprot_seq_R
+        structures["ligand_uniprot_seq"] = uniprot_seq_L
+        for key in [
+            "cluster_id",
+            "cluster_id_R",
+            "cluster_id_L",
+        ]:
+            structures[key] = metadata[key]
+        for metadata_key in [
+            "method",
+            "resolution",
+            "probability",  # probability that the protein complex is a true biological complex
+            "oligomeric_count",
+            "ECOD_names_R",
+            "ECOD_names_L",
+        ]:
+            structures[metadata_key] = metadata[metadata_key]
+        return structures
 
 
 if __name__ == "__main__":
@@ -302,9 +403,8 @@ if __name__ == "__main__":
         index = index[index["id"].isin(args.system_ids)]
     else:
         if args.subset is not None:
-            index = index[
-                (index["subset"] == args.subset) & (index["split"] == args.split)
-            ]
+            assert index["split"] == "test"
+            index = index[(index[f"{args.subset}"]) & (index["split"] == args.split)]
         else:
             index = index[index["split"] == args.split]
         index = index[
@@ -322,12 +422,44 @@ if __name__ == "__main__":
         {
             "id": Value("string"),
             "cluster_id": Value("string"),
+            "pdb_id": Value("string"),
             "complex": ProteinStructureFeature(),
-            "unbound_receptor": ProteinStructureFeature(),
-            "unbound_ligand": ProteinStructureFeature(),
-            "unbound_type": Value("string"),
+            "apo_receptor": ProteinStructureFeature(),
+            "apo_ligand": ProteinStructureFeature(),
+            "pred_receptor": ProteinStructureFeature(),
+            "pred_ligand": ProteinStructureFeature(),
+            "receptor_uniprot_id": Value("string"),
+            "ligand_uniprot_id": Value("string"),
+            "receptor_uniprot_seq": Value("string"),
+            "ligand_uniprot_seq": Value("string"),
+            "receptor_uniprot_resids_with_structure": Array1D("uint16"),
+            "ligand_uniprot_resids_with_structure": Array1D("uint16"),
+            # metadata
+            "oligomeric_count": Value("uint16"),
+            "resolution": Value("float16"),
+            "probability": Value("float16"),
+            "method": Value("string"),
+            # "chain_1": Value("string"),
+            # "chain_2": Value("string"),
+            # "assembly": Value("uint8"),
+            # "asym_id_1": Value("string"),
+            # "asym_id_2": Value("string"),
+            # "ECOD_names_R": Array1D(Value("string")),
+            # "ECOD_names_L": Array1D(Value("string")),
+            # "link_density": Value("float16"),
+            # "planarity": Value("float16"),
+            # "n_residue_pairs": Value("uint16"),
+            # "n_residues": Value("uint16"),
+            # "buried_sasa": Value("float16"),
+            # "intermolecular_contacts": Value("uint16"),  # total number of pair residues with any atom within a %A distance cutoff
+            # "charged_charged_contacts": Value("uint16"),  # total number of pair residues with any atom within a %A distance cutoff
+            # "charged_polar_contacts": Value("uint16"),  # total number of pair residues with any atom within a %A distance cutoff
+            # "charged_apolar_contacts": Value("uint16"),  # total number of pair residues with any atom within a %A distance cutoff
+            # "polar_polar_contacts": Value("uint16"),  # total number of pair residues with any atom within a %A distance cutoff
+            # "apolar_polar_contacts": Value("uint16"),  # total number of pair residues with any atom within a %A distance cutoff
+            # "apolar_apolar_contacts": Value("uint16"),  # total number of pair residues with any atom within a %A distance cutoff
         }
     )
     dataset = Dataset.from_generator(
-        iter(PinderDataset(index, metadata)), features=features
+        iter(PinderDataset(index, metadata, download=args.download)), features=features
     )
