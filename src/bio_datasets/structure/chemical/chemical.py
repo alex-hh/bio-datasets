e37b060629ba62e04cabfc462dcb72b842daf54e
alex-hh <alex.hhooker@gmail.com>
2024-10-29 11:53:47 +0000

    formatting

--- src/bio_datasets/structure/chemical/chemical.py
+++ src/bio_datasets/structure/chemical/chemical.py
@@ -29,7 +29,7 @@ class Molecule(Generic[T]):
 
 class SmallMolecule:
     """A small molecule. This is treated as a single residue in the PDB.
-    
+
     Small molecules in the PDB are cross-referenced with the chemical component dictionary (CCD).
     The three letter 'res_name' is a unique identifier for a chemical component dictionary entry.
     The CCD maps to SMILES and InChI strings, as well as idealised 3D coordinates.
@@ -40,6 +40,7 @@ class SmallMolecule:
 
     TODO: decide whether to load residue dictionary from CCD...
     """
+
     def filter_atoms(self, atoms):
         return atoms[atoms.hetero]
 
