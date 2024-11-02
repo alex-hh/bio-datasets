0ffcb932054ff139b39b8d401e359bde689e2a54
alex-hh <alex.hhooker@gmail.com>
2024-11-02 12:35:31 +0000

    upgrade datasets

--- src/bio_datasets/features/atom_array.py
+++ src/bio_datasets/features/atom_array.py
@@ -21,7 +21,7 @@ from biotite.structure.io.pdbx import CIFFile
 from biotite.structure.residues import get_residue_starts
 from datasets import Array1D, Array2D, config
 from datasets.download import DownloadConfig
-from datasets.features.features import Feature, get_nested_type
+from datasets.features.features import CustomFeature, get_nested_type
 from datasets.table import array_cast, cast_array_to_feature
 from datasets.utils.file_utils import is_local_path, xopen, xsplitext
 from datasets.utils.py_utils import no_op_if_value_is_null, string_to_dict
@@ -310,7 +310,7 @@ def load_structure_from_file_dict(
 
 
 @dataclass
-class AtomArrayFeature(Feature):
+class AtomArrayFeature(CustomFeature):
     """
     AtomArray [`Feature`] to read macromolecular atomic structure data from a PDB or CIF file.
 
@@ -722,7 +722,7 @@ class AtomArrayFeature(Feature):
 
 
 @dataclass
-class StructureFeature(Feature):
+class StructureFeature(CustomFeature):
     """Structure [`Feature`] to read (bio)molecular atomic structure data from supported file types.
     The file contents are serialized as bytes, file path and file type within an Arrow table.
     The file contents are automatically decoded to a biotite AtomArray (if mode=="array") or a
