e74482115dc0ded4a02a3d8af63c139cb6e21585
alex-hh <alex.hhooker@gmail.com>
2024-11-08 10:22:41 +0000

    fix atom array tests

--- tests/features/test_atom_array.py
+++ tests/features/test_atom_array.py
@@ -9,8 +9,8 @@ from bio_datasets.structure.protein import ProteinDictionary
 @pytest.mark.parametrize(
     "feature_kwargs",
     [
-        {"with_element": True, "all_atoms_present": False},
-        {"with_element": False, "all_atoms_present": True},
+        {"with_element": True, "all_atoms_present": False, "load_as": "biotite"},
+        {"with_element": False, "all_atoms_present": True, "load_as": "biotite"},
     ],
 )
 def test_encode_decode_atom_array_with_params(afdb_atom_array, feature_kwargs):
@@ -23,34 +23,11 @@ def test_encode_decode_atom_array_with_params(afdb_atom_array, feature_kwargs):
         assert prot_dict.decode_restype_index(encoded["restype_index"]) == str(
             bs_sequences[0]
         )
-    decoded = feat.decode_example(encoded).atoms
-    # TODO: fix these tests - issue is that atoms get reordered - maybe store perm
-    # assert np.isclose(decoded.coord, afdb_atom_array.coord).all()
-    # assert np.all(decoded.atom_name == afdb_atom_array.atom_name)
-    assert np.all(decoded.res_name == afdb_atom_array.res_name)
-    assert np.all(decoded.res_id == afdb_atom_array.res_id)
-    assert np.all(decoded.chain_id == afdb_atom_array.chain_id)
-
-
-def test_encode_decode_atom_array(afdb_atom_array):
-    """Round-trip encoding of AtomArray.
-    We encode residue-level annotations separately to the atom coords so
-    important to check that they get decoded back to the atom array correctly.
-    """
-    # TODO: test with_element=True, all_atoms_present=False, with_element=False, all_atoms_present=True, with_element=False, all_atoms_present=False
-    prot_dict = ProteinDictionary.from_preset("protein", keep_oxt=True)
-    feat = ProteinAtomArrayFeature(
-        residue_dictionary=prot_dict, all_atoms_present=False
-    )
-    encoded = feat.encode_example(afdb_atom_array)
-    bs_sequences, _ = to_sequence(afdb_atom_array)
-    assert prot_dict.decode_restype_index(encoded["restype_index"]) == str(
-        bs_sequences[0]
-    )
-    decoded = feat.decode_example(encoded).atoms
-    # TODO: fix these tests - issue is that atoms get reordered - maybe store perm
-    # assert np.isclose(decoded.coord, afdb_atom_array.coord).all()
-    # assert np.all(decoded.atom_name == afdb_atom_array.atom_name)
+    decoded = feat.decode_example(encoded)
+    if not feature_kwargs["all_atoms_present"]:
+        # if all_atoms_present is True, atom order will be different
+        assert np.isclose(decoded.coord, afdb_atom_array.coord).all()
+        assert np.all(decoded.atom_name == afdb_atom_array.atom_name)
     assert np.all(decoded.res_name == afdb_atom_array.res_name)
     assert np.all(decoded.res_id == afdb_atom_array.res_id)
     assert np.all(decoded.chain_id == afdb_atom_array.chain_id)
@@ -61,13 +38,13 @@ def test_encode_decode_atom_array_without_residue_dictionary(afdb_atom_array):
     We encode residue-level annotations separately to the atom coords so
     important to check that they get decoded back to the atom array correctly.
     """
-    feat = ProteinAtomArrayFeature(residue_dictionary=None)
+    feat = ProteinAtomArrayFeature(residue_dictionary=None, load_as="biotite")
     encoded = feat.encode_example(afdb_atom_array)
-    decoded = feat.decode_example(encoded).atoms
-    # this time we will drop the OXT atom
-    print(decoded.coord[:10], afdb_atom_array.coord[:10])
-    # assert np.isclose(decoded.coord, afdb_atom_array.coord[:-1]).all()
-    # assert np.all(decoded.atom_name == afdb_atom_array.atom_name[:-1])
-    assert np.all(decoded.res_name == afdb_atom_array.res_name[:-1])
-    assert np.all(decoded.res_id == afdb_atom_array.res_id[:-1])
-    assert np.all(decoded.chain_id == afdb_atom_array.chain_id[:-1])
+    decoded = feat.decode_example(encoded)
+    assert np.isclose(decoded.coord, afdb_atom_array.coord).all()
+    assert np.all(decoded.atom_name == afdb_atom_array.atom_name)
+    assert np.all(decoded.res_name == afdb_atom_array.res_name)
+    assert np.all(decoded.res_id == afdb_atom_array.res_id)
+    assert np.all(decoded.chain_id == afdb_atom_array.chain_id)
+    assert decoded.element[-1] == "O"
+    assert decoded.atom_name[-1] == "OXT"
